6d1d12de9b9aa2f658b7f1cfbf49461b5aa7562e
Doorimul-KJH <junghyub01@gmail.com>
2024-11-06 13:47:46 +0900

    sidebar.html file modified

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1730826366',resources: [
+    cacheName: 'chirpy-1730868375',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
